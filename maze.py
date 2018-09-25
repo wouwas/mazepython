92436f5a04398eb0d548c787e1b05099dc1864a3
Vytautas Ducinskas <vytautasducinskas@Vytautass-MBP.lan>
2018-09-26 00:57:18 +0300

    mazeRoute class added

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 import json
 import  sys
 import os
+import time
 
 class game:
     def __init__(self,response):
@@ -62,11 +63,15 @@ class game:
                #printf(move,possiblemove,i)
                yield possiblemove # return  Cell id
                
-    def printmaze(self,printvalues=0):
+    def printmaze(self,printvalues=0,printcells=None):
+            if printcells is None:
+                printcells=self.cells
             nwall='--'
             wwall='|'
             intersect='+'
             line=''
+            time.sleep(0.01)
+            os.system('cls' if os.name == 'nt' else 'clear')
             for y in  range(0,self.size[1]):
                 for iter in range(1,3):
                     for x in range(0,self.size[0]):
@@ -89,8 +94,8 @@ class game:
                               value=value+'P'
                             if  index==self.endpoint:
                               value=value+'E'
-                            if  index in self.cells.keys() and printvalues==1:
-                              value=value+str(self.cells[index])
+                            if  index in printcells.keys() and printvalues==1:
+                              value=value+str(printcells[index])
                             if len(value)>3:
                               value=value[0:3]
                             elif len(value)<3:
@@ -119,6 +124,11 @@ class game:
             # if domokun to close move away(runaway)
             # if run away choose path with most intersections (how) 
         self.pony=movetocell
+    def makeMoveDomokun(self):
+        charmove=self.cellpossiblemove(self.domokun)
+        movecellsiter={k:v for k,v in self.cells.items() if k in charmove}
+        movetocell=min(movecellsiter,key=movecellsiter.get) #choose shortest path
+        self.domokun=movetocell
     def makeMove (self,start,end):
         self.shortest_path(end)
         self.path=[]
@@ -131,8 +141,63 @@ class game:
         for i in self.path:
             self.cells[i]='%%'
         
-    
-    
+class mazeRoutes:
+
+    def __init__(self,maze):
+        self.maze=maze
+        self.cells={}
+        self.routes=[]
+    def nextCell(self,startpoint,endpoint=None,ForbidenCells=[]):
+        newcellid=startpoint
+        i=0
+        maxlength=0
+        #terminate calculationif start point reached
+        while newcellid>-1 or move==endpoint:
+            i=+1
+            newcellid=-1
+            cels=list([x for x in self.cells.keys() if self.cells[x]==maxlength])
+            maxlength=maxlength+1
+            #print(maxlength)
+            #print(cels,maxlength)
+            for possiblemove in cels:
+               for move in self.maze.cellpossiblemove(possiblemove):
+                   if move>-1 and move not in ForbidenCells:
+                       #print ('Possible to move from ',possiblemove,'to' ,move)
+                       if move not in  self.cells:
+                           self.cells[move]=maxlength+0
+                           newcellid=move
+               #printf(move,possiblemove,i)
+               yield possiblemove # return  Cell id
+               
+    def shortest_path(self,start_point,endpoint=None,ForbidenCells=[]):
+        self.cells[start_point]=0
+        self.maxlength=0
+        for cellid in self.nextCell(start_point,endpoint,ForbidenCells):
+            pass
+    def makeMove (self,start,end,ForbidenCells=[]):
+        self.shortest_path(end,start,ForbidenCells)
+        self.path=[]
+        movetocell=start
+        if start in self.cells.keys():
+            self.path.append(start)
+            for i in range(0,self.cells[movetocell]):            
+                charmove=self.maze.cellpossiblemove(movetocell)
+                movecellsiter={k:v for k,v in self.cells.items() if k in charmove}
+                movetocell=min(movecellsiter,key=movecellsiter.get) #choose shortest path
+                self.path.append(movetocell)
+        #self.cells.clear() #cleanup
+        if start not in self.path:               
+           self.cells[start]='ERR'
+        else:
+           self.cells[start]='ST'
+        self.cells[end]='END'
+
+        for i in self.path:
+            self.cells[i]='%%'
+        for i in ForbidenCells:
+            self.cells[i]='X'
+
+        
                        
         
     
@@ -148,16 +213,31 @@ class game:
 with open('maze.json', 'r') as outfile:
     maze_json=json.load(outfile)
 maze=game(maze_json)
-#maze.shortest_path(maze.endpoint)
-#for i in range(0,maze.pony):
-#    if i%1==0:
-#        maze.printmaze()
-#    maze.makeMovePony()
-#    if maze.cells[maze.pony]==0:
-#        break
-#maze.printmaze()
-maze.makeMove(maze.pony,maze.endpoint)
-maze.printmaze(1)
+##maze.shortest_path(maze.endpoint)
+##for i in range(0,maze.pony):
+##    if i%1==0:
+##        maze.printmaze()
+##    maze.makeMoveDomokun()
+##    maze.makeMovePony()
+##    if maze.cells[maze.pony]==0:
+##        break
+##maze.printmaze()
+mazeroute=mazeRoutes(maze)
+mazeroute.makeMove(0,6)
+maze.printmaze(1,mazeroute.cells)
+time.sleep(4)
+
+mazeroute2=mazeRoutes(maze)
+mazeroute2.makeMove(maze.pony,maze.endpoint,[4,10,34,23])
+maze.printmaze(1,mazeroute2.cells)
+time.sleep(4)
+for i in range(0,len(maze.maze)-1):
+    if 'west' in maze.maze[i] and i%maze.size[0]!=0:
+        maze.maze[i].remove('west')
+    
+mazeroute3=mazeRoutes(maze)
+mazeroute3.makeMove(maze.pony,maze.endpoint,[4,10,34,23])
+maze.printmaze(1,mazeroute3.cells)
 
 
 
